be4e134e0e7ad57f570e07c8f9e1aa2359778267
Pietrocvisentin@gmail.com <Pietrocvisentin@gmail.com>
2024-03-19 20:10:03 -0300

    Meu primeiro commit

---
+++ HelloWord/src/helloworld/Mensagem.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class Mensagem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Meu primeiro programa eclipse");
+	}
+
+}
